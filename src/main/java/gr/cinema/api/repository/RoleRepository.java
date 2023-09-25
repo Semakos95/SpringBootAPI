dceb8f63dacebb56c3b169769e0373850f11b952
Semakos95 <geo.sem@hotmail.com>
2023-09-25 14:35:45 +0300

    fixing necessary method in RoleRepository.java
    //Jwt Filter in progress

--- src/main/java/gr/cinema/api/repository/RoleRepository.java
+++ src/main/java/gr/cinema/api/repository/RoleRepository.java
@@ -1,13 +1,13 @@
 package gr.cinema.api.repository;
 
+import gr.cinema.api.entity.ERole;
 import gr.cinema.api.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
-
+@Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
-    Optional<Role> findByName(String name);
-    boolean existsByName(String name);
+    Optional<Role> findByName(ERole name);
 }
