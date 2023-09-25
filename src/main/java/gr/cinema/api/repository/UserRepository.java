ce41a5ce0b46a147b775611765a1e96a46eff525
Semakos95 <geo.sem@hotmail.com>
2023-09-25 14:28:38 +0300

    delete some unused methods, I make sure I have the 3 necessary methods in UserRepository.java
    //Jwt Filter in progress

--- src/main/java/gr/cinema/api/repository/UserRepository.java
+++ src/main/java/gr/cinema/api/repository/UserRepository.java
@@ -10,11 +10,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByFirstName(String firstName);
-    User findByEmailAndPassword(String email, String password);
-   // User findByFirstName(String firstName);
-    Optional<User> findByEmail(String email);
-    //Optional<User> findByUsernameOrEmail(String username, String email);
-    User findByUsernameAndPassword(String username, String password);
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
